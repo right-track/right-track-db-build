667c878b570270b501bd244622a99a8975e90a62
David Waring <dev@davidwaring.net>
2017-12-07 17:00:11 -0500

    Table: rt_properties

---
+++ src/compile/rt/6-rt_properties.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const config = require('../../../config.json');
+const build = require('../utils/build.js');
+
+/**
+ * rt_properties table definition
+ * @type {RTTableSchema}
+ * @private
+ */
+const TABLE = {
+  name: config.tables.rt.properties,
+  sourceDirectory: config.locations.rtDir,
+  sourceFile: "rt_properties.csv",
+  fields: [
+    {
+      "name": "name",
+      "type": "TEXT",
+      "attributes": "NOT NULL"
+    },
+    {
+      "name": "value",
+      "type": "TEXT",
+      "attributes": "NOT NULL"
+    }
+  ]
+};
+
+
+/**
+ * Build rt_properties table
+ * @type {buildTable}
+ * @private
+ */
+function buildTable(db, agencyOptions, callback) {
+  build.init(db, TABLE, agencyOptions, function() {
+    callback();
+  });
+}
+
+
+
+
+
+module.exports = buildTable;
